7a0b7e4277ea38317a218ebce901541738fbf5a7
jheare <jheare@uw.edu>
2015-03-20 16:16:16 -0700

    Aster Summary Update

--- OlyAsterAttempt.R
+++ OlyAsterAttempt.R
@@ -2,7 +2,7 @@
 require(aster)
 data(echinacea)
 oly<-read.csv('olyaster.csv')
-vars<-c("Live.Dead","Brood","Larvae")
+vars<-c("ld14","brd14","lv14")
 redata<-reshape(oly, varying=list(vars), direction="long",
                 timevar="varb", times=as.factor(vars), v.names="resp")
 names(redata)
@@ -11,8 +11,8 @@ levels(redata$varb)
 redata[42,]
 redata <- data.frame (redata, root=1)
 names(redata)
-pred<-c(0,1,2)
-fam<-c(1,1,1)
+pred<-c(0,0,1)
+fam<-c(1,1,2)
 sapply(fam.default(),as.character)[fam]
 all(pred < seq(along=pred))
 aout1 <- aster(resp~varb+Population+Site, pred, fam, varb, id, root, data=redata)
